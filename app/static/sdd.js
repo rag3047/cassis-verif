6651b9b638f568d4356b145c1bd9473239bf47d9
Gerber Raphael <raphael.gerber@ace.ch>
2023-12-21 17:04:06 +0100

    sdd page

--- app/static/sdd.js
+++ app/static/sdd.js
@@ -3,7 +3,7 @@ const { pdfjsLib } = globalThis;
 let PDF_LOADED = false;
 pdfjsLib.GlobalWorkerOptions.workerSrc = "static/pdfjs/pdf.worker.min.js";
 
-const container = document.querySelector("#viewerContainer");
+const container = document.querySelector(".pdf-viewer-container");
 const eventBus = new pdfjsViewer.EventBus();
 
 // enable hyperlinks within PDF files.
@@ -19,7 +19,15 @@ pdfLinkService.setViewer(pdfViewer);
 // setup pdf viewer events
 eventBus.on("pagesinit", () => {
     PDF_LOADED = true;
-    pdfViewer.currentScaleValue = "page-width";
+    pdfViewer.currentScaleValue = "page-fit";
+});
+
+const page_count = document.querySelector("#page-count");
+const page_number = document.querySelector("#page-num");
+const sel_zoom = document.querySelector("#sel-zoom");
+
+eventBus.on("pagechanging", ({ pageNumber }) => {
+    page_number.value = pageNumber;
 });
 
 // let skipNextEvent = false;
@@ -65,31 +73,73 @@ eventBus.on("pagesinit", () => {
 //     }
 // });
 
-async function mapPdfOutlineToPages(outline, pdf) {
-    const pairs = [];
-    if (!outline) return pairs;
+// async function mapPdfOutlineToPages(outline, pdf) {
+//     const pairs = [];
+//     if (!outline) return pairs;
 
-    for (let o of outline) {
-        const ref = o.dest[0];
-        const page_idx = await pdf.getPageIndex(ref);
-        pairs.push({ title: o.title, page_number: parseInt(page_idx) + 1, dest: o.dest });
+//     for (let o of outline) {
+//         const ref = o.dest[0];
+//         const page_idx = await pdf.getPageIndex(ref);
+//         pairs.push({ title: o.title, page_number: parseInt(page_idx) + 1, dest: o.dest });
 
-        if (o.items) {
-            pairs.push(...(await mapPdfOutlineToPages(o.items)));
-        }
-    }
+//         if (o.items) {
+//             pairs.push(...(await mapPdfOutlineToPages(o.items)));
+//         }
+//     }
 
-    return pairs;
-}
+//     return pairs;
+// }
 
+let pdf;
 async function load_document() {
     // Load document.
-    const pdf = await pdfjsLib.getDocument("static/CaSSIS-FSW-DDD_v3.pdf").promise;
+    pdf = await pdfjsLib.getDocument("static/CaSSIS-FSW-DDD_v3.pdf").promise;
     pdfViewer.setDocument(pdf);
     pdfLinkService.setDocument(pdf, null);
 
+    page_count.textContent = pdf.numPages;
+
     // get document outline
-    const outline = await mapPdfOutlineToPages(await pdf.getOutline(), pdf);
+    // const outline = await mapPdfOutlineToPages(await pdf.getOutline(), pdf);
 }
 
-document.addEventListener("DOMContentLoaded", load_document);
+async function set_zoom() {
+    if (!PDF_LOADED) return;
+    pdfViewer.currentScaleValue = sel_zoom.value;
+}
+
+async function zoom_in() {
+    if (!PDF_LOADED) return;
+    pdfViewer.currentScaleValue = (parseFloat(pdfViewer.currentScaleValue) || 1) + 0.1;
+    // TODO: Update Select Value
+}
+
+async function zoom_out() {
+    if (!PDF_LOADED) return;
+    pdfViewer.currentScaleValue = (parseFloat(pdfViewer.currentScaleValue) || 1) - 0.1;
+    // TODO: Update Select Value
+}
+
+async function set_page_number(event) {
+    if (!PDF_LOADED) return;
+
+    if (event.key == "Enter") {
+        const page = Math.min(page_number.value, pdf.numPages);
+        pdfViewer.currentPageNumber = page;
+    }
+}
+
+async function next_page() {
+    if (!PDF_LOADED) return;
+    pdfViewer.nextPage();
+}
+
+async function prev_page() {
+    if (!PDF_LOADED) return;
+    pdfViewer.previousPage();
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    NiceSelect.bind(document.querySelector("select"), { searchable: false });
+    await load_document();
+});
